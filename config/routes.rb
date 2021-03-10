b2901ceaea8f3c941703230bfd65b24ff2e5858e
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-10 16:49:02 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :books
 
+  # ユーザー新規登録
   get "/sign_up", to: "users#new"
   post "/sign_up", to: "users#create"
+  # ログイン・ログアウト
+  get "/sign_in", to: "session#new"
+  post "/sign_in", to: "session#create"
+  get "/sign_out", to: "session#destroy"
 
 end
